7b1509dcd41c23c8099735ec8c312868ab50a8f1
nicolasjm0702 <70339033+nicolasjm0702@users.noreply.github.com>
2023-01-27 15:35:49 -0300

    simplificado

--- gerar-nove.py
+++ gerar-nove.py
@@ -1,51 +1,14 @@
-palavra = str(input("Palavra ou caractere : "))
-max = int(input("Maximo de caracteres: "))
+def main():
+    palavra = str(input("Digite uma palavra: "))
+    maximo_caracteres = int(input("Digite o número máximo de caracteres: "))
+    texto = ""
+    while len(texto) < maximo_caracteres:
+        texto += palavra + "? "
+        palavra += palavra
+    texto = texto[:(maximo_caracteres - 1)] + "?"
+    print(texto)
 
-def GerarNove(palavra, max):
-    lista = []
-    posAtual = int(1)
-    resultado = str('')
-    posSubstituida = 1
-    outraPosSubstituida = 2
-    x = 4
-    y = 4
 
-    while posAtual < max:
-        lista.append(palavra)
-        posAtual = posAtual + 1
-
-    while posSubstituida < max ^ 2:
-        lista.insert(posSubstituida, "?")
-        posSubstituida = posSubstituida + x
-        x = x + 1
-        lista.insert(outraPosSubstituida, " ")
-        outraPosSubstituida = outraPosSubstituida + y
-        y = y + 1
-
-    lista.append("? ")
-
-    while len(lista) > max:
-        lista.pop(max)
-
-    for i in lista:
-        resultado = f"{resultado}" + f"{i}"
-
-    while len(resultado) > max:
-        resultado = resultado[:-1]
-
-    resultado = f"{resultado}?"
-
-    if len(resultado) > max:
-        char = resultado[-2]
-        resultado = resultado[::-1].replace(char[::-1], ""[::-1], 1)[::-1]
-        resultado = resultado.replace("? ? ", "? ")
-        resultado = resultado.replace("? ? ", "?")
-        resultado = resultado.replace("??", "?")
-        resultado = resultado.replace("? ?", "?")
-        print(resultado)
-    else:
-        print(resultado)
-
-GerarNove(palavra, max)
-
-pause = input(" ")
+if __name__ == "__main__":
+    main()
+    pause = input("")
